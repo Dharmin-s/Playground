c4b76d2d7e512d75ade8aedb630caf319f68dab1
Dharmin-s <49226422+Dharmin-s@users.noreply.github.com>
2019-06-19 09:03:35 +0530

    Test code pushed to repository.

---
+++ Perimeter and area of a rectangle/Main.java
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int a = 54;
+  int p = 30;
+  printf("The perimeter of the rectangle is: %d cm",p);
+  printf("\nThe area of the rectangle is: %d sq cm",a);
+  return 0;
+}
---
+++ Print the quotient and remainder/Main.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a=365;
+  int b=4;
+  int c=a/b;
+  int d=a%b;
+  printf("Quotient: %d",c);
+  printf("\nRemainder: %d",d);
+  return 0;
+}
